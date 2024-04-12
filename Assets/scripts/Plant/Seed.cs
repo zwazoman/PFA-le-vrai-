27649c5cc9d0e81fe2f74cef9af9fdca62fb22c1
zwazoman <gloupik.lemaire@gmail.com>
2024-04-12 11:15:05 +0200

    avanée plantes et tools
    harvest en cours

--- Assets/scripts/Plant/Seed.cs
+++ Assets/scripts/Plant/Seed.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Seed : Item
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [field : SerializeField]
+    public GameObject Plant { get; private set; }
 }
