68bc145babaf820bbee2e60465aac90f3e1da067
zwazoman <gloupik.lemaire@gmail.com>
2024-04-12 12:49:54 +0200

    scene et fix harvest
    pas loin des plantes fonctionnelles

--- Assets/scripts/Plant/PlantHarvest.cs
+++ Assets/scripts/Plant/PlantHarvest.cs
@@ -7,11 +7,18 @@ public class PlantHarvest : MonoBehaviour
     public bool isHarvesteable { get; set; }
     [SerializeField] PlantMain _main;
 
+    private void Awake()
+    {
+        isHarvesteable = false;
+    }
     public void Harvest()
     {
+        print("try harvest");
         if (!isHarvesteable) return;
+        print("harvest");
         Instantiate(_main.orb, transform.position, Quaternion.identity);
         _main.PlantField.IsEmpty = true;
+        _main.PlantField.Plow();
         Destroy(gameObject);
     }
 }
