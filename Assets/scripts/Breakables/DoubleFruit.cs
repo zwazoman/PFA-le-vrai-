f3a90cf9a19345a84f578fb0bb7094cab718a647
zwazoman <gloupik.lemaire@gmail.com>
2024-04-12 21:49:00 +0200

    débuts breakables

---
+++ Assets/scripts/Breakables/DoubleFruit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleFruit : Breakable
+{
+    public override void Break()
+    {
+        base.Break();
+    }
+}
---
+++ Assets/scripts/Breakables/HolyFlask.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolyFlask : Breakable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
