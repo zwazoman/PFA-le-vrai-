8ed1fa0881587ce24f101409132043d56d6f5c97
zwazoman <gloupik.lemaire@gmail.com>
2024-04-12 11:40:58 +0200

    plant harvest kasi good

--- Assets/scripts/Plant/PlantHarvest.cs
+++ Assets/scripts/Plant/PlantHarvest.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlantHarvest : MonoBehaviour
 {
     public bool isHarvesteable { get; set; }
+    [SerializeField] PlantMain _main;
 
     public void Harvest()
     {
-
+        if (!isHarvesteable) return;
+        Instantiate(_main.orb, transform.position, Quaternion.identity);
+        _main.PlantField.IsEmpty = true;
+        Destroy(gameObject);
     }
 }
