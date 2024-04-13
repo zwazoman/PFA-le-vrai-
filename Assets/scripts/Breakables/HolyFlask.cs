eda73a9fa5310915d330a1cac3fbfe9124fef516
zwazoman <gloupik.lemaire@gmail.com>
2024-04-13 13:58:46 +0200

    breakables et taille des outils fixed

--- Assets/scripts/Breakables/HolyFlask.cs
+++ Assets/scripts/Breakables/HolyFlask.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class HolyFlask : Breakable
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Break()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        base.Break();
     }
 }
