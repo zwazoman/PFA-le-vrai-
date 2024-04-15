51b21c6e22f8974162d91f34c3b0091a81f8404e
zwazoman <gloupik.lemaire@gmail.com>
2024-04-15 14:01:34 +0200

    relations intéractions et outils good

--- Assets/scripts/cameraBehaviour.cs
+++ Assets/scripts/cameraBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraBehaviour : MonoBehaviour
+public class CameraBehaviour : MonoBehaviour
 {
     GameObject target;
     Vector3 vel;
