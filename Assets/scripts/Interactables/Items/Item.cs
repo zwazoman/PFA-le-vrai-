dce8863dad83594d3f1e20b73ebf425b6c6de489
zwazoman <gloupik.lemaire@gmail.com>
2024-04-17 02:12:00 +0200

    rien nul

--- Assets/scripts/Interactables/Items/Item.cs
+++ Assets/scripts/Interactables/Items/Item.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(ItemJump))]
 /// <summary>
 /// permet au joueur de rammasser l'objet dans ses mains
 /// hérite de la classe "Interactable"
--- Assets/scripts/Interactables/Items/WheelBarrow.cs
+++ Assets/scripts/Interactables/Items/WheelBarrow.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class WheelBarrow : Interactable
 {
+    //[SerializeField] float _distanceToLink;
     public override void InteractWith()
     {
         PlayerMain.Instance.WheelBarrow.Equip();
+        /*transform.position = PlayerMain.Instance.gameObject.transform.position + transform.forward * _distanceToLink;
+        transform.rotation = PlayerMain.Instance.gameObject.transform.rotation;*/
     }
 }
