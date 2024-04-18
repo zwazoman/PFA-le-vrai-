41a19d04227b6c5088bb3d3aba554da06e377948
AsaeNeopie <n.noahchery@gmail.com>
2024-04-18 16:37:37 +0200

    Preparation du shop

--- Assets/scripts/Interactables/Shop.cs
+++ Assets/scripts/Interactables/Shop.cs
@@ -10,11 +10,26 @@ public class Shop : Interactable
     [SerializeField] TMP_Text _description;
     [SerializeField] TMP_Text _prices;
     [SerializeField] TMP_Text _name;
+    [SerializeField] PlayerStats _pStats;
     public override void InteractWith()
     {
         _panelItem.SetActive(!false);
         _description.text = _soItem.Description;
         _prices.text = _soItem.Price.ToString();
         _name.text = _soItem.Name;
+    }
+
+    public void BuyItem()
+    {
+        if (_pStats.Money >= _soItem.Price)
+        {
+            Debug.Log("Objets acheté");
+            //Faire spawn l'objet a
+        }
+        else if (_pStats.Money <= _soItem.Price)
+        {
+            Debug.Log("Pas les sous");
+            //Dire que tu n'a pas de sous 
+        }
     }
 }
