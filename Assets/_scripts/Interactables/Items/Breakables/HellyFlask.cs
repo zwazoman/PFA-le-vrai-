e327942da55b84ce99f712a4430d312c7fdcd182
zwazoman <gloupik.lemaire@gmail.com>
2024-05-06 11:20:26 +0200

    update input et flasks qui fonctionnent  a peu pres

---
+++ Assets/_scripts/Interactables/Items/Breakables/HellyFlask.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellyFlask : Breakable
+{
+    [SerializeField] private float _range;
+    [SerializeField] private float _increaseAmount;
+
+    private Collider[] _hitColliders;
+
+    public override void Break()
+    {
+        //instancier vfx
+        _hitColliders = Physics.OverlapSphere(transform.position, _range);
+        foreach (Collider hitCollider in _hitColliders)
+        {
+            if (hitCollider.gameObject.TryGetComponent<PlantMain>(out PlantMain main))
+            {
+                //rajouter de la corruption ?
+            }
+        }
+        base.Break();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _range);
+    }
+}
