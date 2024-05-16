c2e23dca8c99634459c9f0cef644b53e3d5abf36
AsaeNeopie <n.noahchery@gmail.com>
2024-05-16 17:29:42 +0200

    UI mes couilles

--- Assets/_scripts/UI/GameplayUI/GameplayPanel.cs
+++ Assets/_scripts/UI/GameplayUI/GameplayPanel.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
+using UnityEngine.InputSystem.XR.Haptics;
 
 public class GameplayPanel : MonoBehaviour
 {
-    public TMP_Text TextMoney;
+    [SerializeField] private TMP_Text _textMoney;
 
+    
+    public void UpdateDisplay(float from, float to)
+    {
+        StartCoroutine(Nathan.InterpolateOverTime(from, to, 0.2f,UpdateText));
+    }
 
+    private void UpdateText(float newValue)
+    {
+        _textMoney.text = Mathf.Round(newValue).ToString();
+    }
     public void Start()
     {
-        TextMoney.text = PlayerMain.Instance.Stats.Money.ToString();
+        _textMoney.text = PlayerMain.Instance.Stats.Money.ToString();
+    }
+
+    public void Display(float current,float target)
+    {
+        DOVirtual.Float(current, target, 0.5f, (x) =>
+        {
+            current = x;
+        }).OnComplete(() => { Display(current, target); });
     }
 }
 
