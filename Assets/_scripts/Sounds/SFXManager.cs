de2fdbaf8725377c64a2ace207ef9117e18358dc
AsaeNeopie <n.noahchery@gmail.com>
2024-05-16 13:51:41 +0200

    merge main

--- Assets/_scripts/Sounds/SFXManager.cs
+++ Assets/_scripts/Sounds/SFXManager.cs
@@ -23,12 +23,13 @@ public class SFXManager : MonoBehaviour
         }
     }
 
-    public void PlaySFXClip(AudioClip[] audioClips, Transform spawnTransform, float volume)
+    public void PlaySFXClip(AudioClip[] audioClips, Transform spawnTransform, float volume, float pitch = 1f)
     {
         int rand = Random.Range(0, audioClips.Length);
         AudioSource audioSource = Instantiate(_SFXObject, spawnTransform.position, Quaternion.identity);
         audioSource.clip = audioClips[rand];
         audioSource.volume = volume;
+        audioSource.pitch = pitch;
         audioSource.Play();
         float clipLength = audioSource.clip.length;
         Destroy(audioSource.gameObject, clipLength);
