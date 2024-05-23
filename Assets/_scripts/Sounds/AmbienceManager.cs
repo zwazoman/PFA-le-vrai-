2d0639807820228c13943631f0cf77aa092ac4ba
zwazoman <gloupik.lemaire@gmail.com>
2024-05-23 16:16:40 +0200

    ambience manager fonctionne

--- Assets/_scripts/Sounds/AmbienceManager.cs
+++ Assets/_scripts/Sounds/AmbienceManager.cs
@@ -4,12 +4,43 @@ using UnityEngine;
 
 public class AmbienceManager : MonoBehaviour
 {
-    [SerializeField] AudioClip[] _ambientSound1;
-    [SerializeField] AudioClip[] _volume1;
+    [SerializeField] float _minTime;
+    [SerializeField] float _maxTime;
+
+    [SerializeField] float _sphereSize = 10f;
+
+    [SerializeField] AudioClip[] _ambientSound;
+    [SerializeField] float _ambiendVolume;
+
+    private void Start()
+    {
+        StartCoroutine(PlayAmbientSound());
+    }
+
+    IEnumerator PlayAmbientSound()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(_minTime, _maxTime));
+            SFXManager.Instance.PlaySFXClip(_ambientSound, SelectSoundSpot(), _ambiendVolume);
+        }
+    }
 
     Vector3 SelectSoundSpot()
     {
-        Random.insideUnitSphere
+        Vector3 spot = Random.insideUnitSphere * _sphereSize;
+        Mathf.Abs(spot.y);
+        return transform.position + spot;
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(PlayAmbientSound());
     }
 
 }
