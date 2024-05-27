5780164c409b2e4e97fc349a22e54237f7c2f513
AsaeNeopie <n.noahchery@gmail.com>
2024-05-27 12:28:02 +0200

    Dialogue pt 1

--- Assets/_scripts/DialogueSystem/dialogues/Df_Geoffrus.cs
+++ Assets/_scripts/DialogueSystem/dialogues/Df_Geoffrus.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 public class Df_Geoffrus : DialogueFlow
 {
     public Df_Geoffrus(DialoguePanel panel, DialogueCharacters characters, MonoBehaviour WorldObject) : base(panel, characters, WorldObject) { }
-
+    bool _firstTalk;
     public override async Task StartDialogue()
     {
         _panel.InitDialogue(_characters.Geoffrrus, _characters.Bobbus);
-
-        await _characters.Geoffrrus.Say("Blabla bla bla");
-        await _characters.Geoffrrus.Say("Louis est gay");
+        if (_firstTalk)
+        {
+            await _characters.Geoffrrus.Say("Oh ? Et bien, si je m'y attendais... Cela faisais bien longtemps que je n'avais pas reçu de visite !");
+            int result = await _characters.Geoffrrus.Ask("Qui êtes vous, donc, brave âme ? ", new string[] { "Vous êtes... un genre de démon ?", "Je suis le fermier, j'habite au bout du chemin.", "Bobbus. Je suis venu avec Charon il y a peu...", "Au revoir." });
+            await _characters.Bobbus.Say("Louis est gay");
+        }
     }
 }
     
