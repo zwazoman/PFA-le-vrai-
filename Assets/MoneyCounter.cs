4e07c76027008a30ae148b0fe374541f2803e74d
nathan-exe <nathan.tazi.1@gmail.com>
2024-05-28 17:42:18 +0200

    debut bulle fleche

--- Assets/MoneyCounter.cs
+++ Assets/MoneyCounter.cs
@@ -1,12 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MoneyCounter : MonoBehaviour
 {
     int money;
-    public void SetMoney(float from, float to)
-    {
+    [SerializeField] TMP_Text text;
 
+    [SerializeField] AnimationCurve scaleCurve;
+    float scale = 1;
+
+    private void Start()
+    {
+        money = PlayerMain.Instance.Stats.Money;
+        text.text = money.ToString();
+    }
+    public void SetMoney(float to)
+    {
+        StartCoroutine(Nathan.InterpolateOverTime(money, to, 0.5f, ApplyText, Nathan.SmoothStep01));
+    }
+
+    void applyScale(float a)
+    {
+        scale = Mathf.Lerp(scale, 1, scaleCurve.Evaluate(a));
+        transform.localScale = Vector3.one * scale;
+    }
+
+    void ApplyText(float a)
+    {
+        money = (int) a;
+        text.text = money.ToString();
     }
 }
