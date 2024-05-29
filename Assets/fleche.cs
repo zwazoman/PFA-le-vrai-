3c20ceb61aea323fab0917ec7f2667b8c5de98ac
Nathan <63930671+nathan-exe@users.noreply.github.com>
2024-05-30 00:21:06 +0200

    m'attendez pas demain

--- Assets/fleche.cs
+++ Assets/fleche.cs
@@ -1,3 +1,4 @@
+using Cinemachine.Utility;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,40 +16,53 @@ public class fleche : MonoBehaviour
     [SerializeField] float Margin;
     [SerializeField] float Frequency;
     [SerializeField] float Amplitude;
-    public void setImage(Sprite sprite)
+
+    [SerializeField] Sprite CharonSprite;
+    [SerializeField] Sprite MoulinSprite;
+
+    public static fleche instance { get; private set; }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void OnDestroy()
+    {
+        instance = null;
+    }
+
+
+    public void SetUp(Sprite sprite, Transform newTarget)
     {
         image.sprite = sprite;
-    }
-
-    public void SetTarget()
-    {
-
-    }
-
-    public void Disable()
-    {
-        
+        target = newTarget;
+        gameObject.SetActive(true);
     }
 
     private void Update()
     {
+        if (target == null) return;
+        
+        //position
+        Vector2 targetPosition = CameraBehaviour.Instance.cam.WorldToScreenPoint(target.position);
 
-        if(ShouldFollow||true)
+        Vector2 endPosition = targetPosition;
+        endPosition.x = Mathf.Clamp(endPosition.x, Margin,Screen.width-Margin);
+        endPosition.y = Mathf.Clamp(endPosition.y, Margin,Screen.height-Margin);
+        GetComponent<RectTransform>().anchoredPosition = endPosition;
+
+        if ((endPosition - new Vector2(Screen.width, Screen.height)).sqrMagnitude < 100 * 100)
         {
-            //position
-            Vector2 targetPosition = CameraBehaviour.Instance.cam.WorldToScreenPoint(target.position);
-
-            Vector2 endPosition = targetPosition;
-            endPosition.x = Mathf.Clamp(endPosition.x, Margin,Screen.width-Margin);
-            endPosition.y = Mathf.Clamp(endPosition.y, Margin,Screen.height-Margin);
-            GetComponent<RectTransform>().anchoredPosition = endPosition;
-
-            //rotation
-            Vector2 JENPEUPLU = targetPosition - new Vector2(Screen.width, Screen.height) / 2f;
-            LaFleche.rotation = Quaternion.Euler(Vector3.forward * (Mathf.Atan2(JENPEUPLU.y,JENPEUPLU.x)*Mathf.Rad2Deg+90));
-
-            //scale
-            transform.localScale = Vector3.one * (1f + Mathf.Sin(Time.time * Frequency) * Amplitude);
+            target = null;
+            gameObject.SetActive(false);
         }
+
+        //rotation
+        Vector2 JENPEUPLU = targetPosition - new Vector2(Screen.width, Screen.height) / 2f;
+        LaFleche.rotation = Quaternion.Euler(Vector3.forward * (Mathf.Atan2(JENPEUPLU.y,JENPEUPLU.x)*Mathf.Rad2Deg+90));
+
+        //scale
+        transform.localScale = Vector3.one * (1f + Mathf.Sin(Time.time * Frequency) * Amplitude);
+        
     }
 }
