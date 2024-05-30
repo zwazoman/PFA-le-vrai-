f36b594c36602588b0259ec4aea2db5bafd09ad3
zwazoman <gloupik.lemaire@gmail.com>
2024-05-30 11:55:42 +0200

    init audiomixer

---
+++ Assets/_scripts/Sounds/AudioMixerManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioMixerManager : MonoBehaviour
+{
+    [SerializeField] AudioMixer _mixer;
+
+    public void SetMasterVolume(float level)
+    {
+
+    }
+
+    public void SetSFXVOlume(float level)
+    {
+
+    }
+
+    public void SetMusicVolume(float level)
+    {
+
+    }
+}
