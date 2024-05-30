9912be287360b38f1f5704ca9c4b4b7b902d4ebc
Lisvald <louis.espi22@gmail.com>
2024-05-30 16:44:08 +0200

    Changements dialogues Eve Tuto Charon

--- Assets/_scripts/DialogueSystem/dialogues/Df_Eve.cs
+++ Assets/_scripts/DialogueSystem/dialogues/Df_Eve.cs
@@ -1,23 +1,43 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 
 public class Df_Eve : DialogueFlow
 {
     public Df_Eve(DialoguePanel panel, DialogueCharacters characters, MonoBehaviour WorldObject) : base(panel, characters, WorldObject) { }
+
+    List<string> dialogueEve = new List<string>()
+
+    {
+        "Ne me fais pas perdre mon temps si vous n'êtes pas encore prêt, mortel" ,
+        "Vous n'avez pas des champs à bêcher ?",
+        "Je sais que vous avez autres choses à faire que de m'ennuyer",
+        "Votre âme ne va pas se racheter toute seule si vous restez ici",
+        "Vous n'avez rien de mieux à faire de vos journées ?",
+        "Je suis sûre d'avoir entendu la cloche de Charon. Vous devriez aller voir."
+
+
+    };
+
+
     public override async Task StartDialogue()
     {
         _panel.InitDialogue(_characters.Eve, _characters.Bobbus);
 
-        await _characters.Eve.Say("Blabla bla bla");
-        int result = await _characters.Eve.Ask("Tu savais que Louis etais gay ?", new string[] { "Oui", "Non" });
+        await _characters.Bobbus.Say("Bite");
+
+        int result = await _characters.Eve.Ask($"Alors, prêt à racheter ton âme ? Elle t'en coûtera [777] âmes", new string[] { "Oui", "Non" });
 
         if (result == 0)
         {
-            await _characters.Eve.Say("UwU");
+            await _characters.Eve.Say("Tes efforts pour te racheter t'ont amenés à cet instant, mortel. Soit récompensé pour ton dévouement");
         }
         else
+         
         {
-            await _characters.Eve.Say("Maintenant tu le sais");
+
+            await _characters.Eve.Say(dialogueEve[Random.Range(0,dialogueEve.Count)]);
+
         }
         
     }
