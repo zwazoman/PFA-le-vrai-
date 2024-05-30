1ee5e4a803abc5bf2a3f15bae64913d844afdfb1
nathan-exe <nathan.tazi.1@gmail.com>
2024-05-30 16:24:41 +0200

    fix couleurs questions

---
+++ Assets/VelocityTrigger.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VelocityTrigger : MonoBehaviour
+{
+    [SerializeField] float treshold = 0.2f;
+    public bool isMoving = false;
+
+    Vector3 OldPosition ;
+
+    public UnityEvent OnMove;
+    public UnityEvent OnStopMoving;
+    // Update is called once per frame
+    void Update()
+    {
+        float sqrMagnitude = (transform.position - OldPosition).sqrMagnitude;
+        bool m = sqrMagnitude * sqrMagnitude > treshold * treshold;
+
+        if(m!=isMoving)
+        {
+            if (m)
+            {
+                OnMove?.Invoke();
+            }
+            else if (!m)
+            {
+                OnStopMoving?.Invoke();
+            }
+
+            isMoving = m;
+        }
+        
+    }
+
+    private void LateUpdate()
+    {
+        OldPosition = transform.position;
+    }
+}
