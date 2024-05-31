bdc161d7e965da938839b3668d82458f97b4d034
nathan-exe <nathan.tazi.1@gmail.com>
2024-05-31 14:28:43 +0200

    quetes + bulle de con

---
+++ Assets/Quest.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Quest : MonoBehaviour
+{
+    [SerializeField] string _name;
+    int _progress = 0;
+    [SerializeField] int _progressEndTreshold;
+    bool _completed = false;
+    List<string> _nextQuests = new List<string>();
+    TMP_Text _textComponent;
+    [SerializeField] string text;
+
+    public void AddProgress(int amount = 1)
+    {
+        if (_completed || !gameObject.activeSelf) { return; }
+
+        _progress += amount;
+        UpdateUI();
+
+        if( _progress >= _progressEndTreshold)
+        {
+            //montrer les quetes suivantes
+            foreach ( string q in _nextQuests)
+            {
+                Assert.IsTrue(QuestManager.Instance.Quests.Keys.Contains(q),"Cette quete existe pas gros fils de pute de merde");
+                QuestManager.Instance.Quests[q].gameObject.SetActive(true);
+            }
+
+            //enlever cette quete
+            Destroy(gameObject);
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        QuestManager.Instance.Quests.Remove(_name);
+    }
+
+    private void UpdateUI()
+    {
+        _textComponent.text =  text + (_progressEndTreshold >1 ?  $" ({_progress}/{_progressEndTreshold}) ":"");
+    }
+
+}
