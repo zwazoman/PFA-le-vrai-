e2a1c6ede68dcc5fe720c3cfda6997a069a2a5d4
Nathan <63930671+nathan-exe@users.noreply.github.com>
2024-06-01 23:00:19 +0200

    La putain de brouette Enfin

--- Assets/_scripts/3 Cs/Player/WheelBarrow/WheelBarrowVisual.cs
+++ Assets/_scripts/3 Cs/Player/WheelBarrow/WheelBarrowVisual.cs
@@ -3,20 +3,40 @@ using UnityEngine;
 public class WheelBarrowVisual : MonoBehaviour
 {
     public GameObject Barrow;
-    public GameObject Wheel;
     public float wheelRadius;
-    public float maxAngle;
+    public float maxSlopeAngle = 35f;
     public float kekchos;
     public float truk;
+
+    public float angleOffet = 0;
+    public Vector3 localForward;
     [SerializeField] LayerMask layerMask;
 
+    public float angle;
     private void Update()
     {
+        if (transform.parent == null) return;
         RaycastHit hit;
-        Debug.DrawRay(/*transform.position + transform.forward * truk*/ Wheel.transform.position + Vector3.up * kekchos, Vector3.down * kekchos * 2, Color.yellow);
-        if(Physics.Raycast(/*transform.position + transform.forward * truk*/Wheel.transform.position + Vector3.up * kekchos, Vector3.down,out hit, kekchos * 2, layerMask))
+        Transform p = transform.parent;
+
+        Debug.DrawRay(/*transform.position + transform.forward * truk*/ p.position - p.forward * truk + Vector3.up * kekchos, Vector3.down * kekchos * 2, Color.yellow);
+        if(Physics.Raycast(/*transform.position + transform.forward * truk*/p.position - p.forward * truk + Vector3.up * kekchos, Vector3.down,out hit, kekchos * 2, layerMask))
         {
-           Barrow.transform.forward = (-hit.point + Barrow.transform.position) - Vector3.up * wheelRadius;
+            localForward = (hit.point - Barrow.transform.position) + Vector3.up * wheelRadius;
+            Debug.DrawRay(/*transform.position + transform.forward * truk*/ Barrow.transform.position, localForward, Color.red);
+
+            localForward = p.transform.InverseTransformVector(localForward);
+            //Vector3 a = Barrow.transform.localEulerAngles;
+            angle = - Mathf.Atan2(localForward.y, localForward.z)* Mathf.Rad2Deg;
+            angle = angle > 0 ? angle : angle + 360f;
+            print(angle);
+
+            if (angle > 180f+maxSlopeAngle || angle < 180f-maxSlopeAngle) return;
+            angle += angleOffet;
+            angle = angle % 360;
+            //angle = Mathf.Clamp(angle, minAngle, maxAngle);
+            transform.localRotation = Quaternion.Euler( Vector3.right * angle);
+            //Barrow.transform.localEulerAngles = new Vector3(/*Mathf.Clamp( a.x,minAngle,maxAngle)*/Mathf.Rad2Deg * Mathf.Atan2(localForward.y, localForward.z)+ angleOffet, 0, 0);
         }
     }
 }
