1e9920aa28ffe4d76fc7a95f4487f941610cf4a4
Lisvald <louis.espi22@gmail.com>
2024-06-06 00:12:44 +0200

    Oulala les bêtises

--- Assets/_scripts/DialogueSystem/dialogues/Df_Bed.cs
+++ Assets/_scripts/DialogueSystem/dialogues/Df_Bed.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -5,12 +6,28 @@ public class Df_Bed : DialogueFlow
 {
     public Df_Bed(DialoguePanel panel, DialogueCharacters characters, MonoBehaviour WorldObject) : base(panel, characters, WorldObject) { }
 
+    List<string> dialogueNarrator = new List<string>()
+
+    { "Votre lit, moelleux et attrayant" ,
+        "Votre lit ne vous a jamais paru si attirant",
+        "Je suis ton lit. Viens dans mes bras, Bobbus" , 
+        "Un beau lit moelleux" ,
+        "Dormir ne vous a jamais fait autant envie" ,
+        "Un brin de repos ne vous ferait sans doute pas de mal" ,
+        "L'endroit parfait pour vous reposer.",
+        "Vous n'avez plus qu'à poser la tête sur votre oreiller et vous endormir"
+       
+
+
+    };
+
     public override async Task StartDialogue()
     {
         _panel.InitDialogue(_characters.Narrator, _characters.Bobbus);
 
-        await _characters.Narrator.Say("Votre lit, moelleux et attrayant");
-        int result = await _characters.Narrator.Ask("Souhaitez vous dormir ?", new string[] { "Oui", "Non"});
+        await _characters.Narrator.Say(dialogueNarrator[Random.Range(0, dialogueNarrator.Count)]);
+
+        int result = await _characters.Narrator.Ask("Louis est-il ?", new string[] { "Oui", "Non"});
 
         if (result == 0) 
         {
@@ -21,4 +38,7 @@ public class Df_Bed : DialogueFlow
            
         }
     }
+
 }
+
+
--- Assets/_scripts/DialogueSystem/dialogues/ItemShop/Df_Shop_Shop_Seed.cs
+++ Assets/_scripts/DialogueSystem/dialogues/ItemShop/Df_Shop_Shop_Seed.cs
@@ -9,25 +9,25 @@ public class Df_Shop_Shop_Seed : DialogueFlow
     {
         _panel.InitDialogue(_characters.Geoffrrus, _characters.Bobbus);
 
-        await _characters.Geoffrrus.Say("#Une graine# en plus pour votre labeur, que vous pouvez emportez de suite. Vous n’avez qu’à sélectionner un des trois types disponibles.");
-        await _characters.Geoffrrus.Say($"Je vous en demande seulement quelques #âmes, {((SellingSpot)WorldObject).price}.#");
+        await _characters.Geoffrrus.Say("#Une graine# en plus pour votre labeur, que vous pouvez emportez de suite.");
+        await _characters.Geoffrrus.Say($"Je vous en demande seulement quelques #poussières d'âmes {((SellingSpot)WorldObject).price}.#");
 
-        int resultat = await _characters.Narrator.Ask($"Voulez vous cette #graine# pour #{((SellingSpot)WorldObject).price} âmes.#?", new string[] { "J'achète !", "J'ai changé d'avis." });
+        int resultat = await _characters.Narrator.Ask($"Voulez vous cette #graine# pour #{((SellingSpot)WorldObject).price} poussières d'âmes.#?", new string[] { "J'achète !", "J'ai changé d'avis." });
         if (resultat == 0)
         {
             if (((SellingSpot)WorldObject).price <= PlayerMain.Instance.Stats.Money)
             {
-                await _characters.Geoffrrus.Say("Bien. Prenez cette âme, et faite votre devoir, fermier.");
+                await _characters.Geoffrrus.Say("Très bien, elle est à vous.");
                 WorldObject.SendMessage("SellItem");
             }
             else
             {
-                await _characters.Geoffrrus.Say("Une telle faveur n’est pas sans coût. Revenez lorsque vous pourrez vous l’offrir");
+                await _characters.Geoffrrus.Say("Revenez lorsque vous pourrez vous l’offrir");
             }
         }
         else
         {
-            await _characters.Geoffrrus.Say("Très bien.");
+            await _characters.Geoffrrus.Say("Cette âme attendra son tour ici dans ce cas.");
         }
     }
 }
