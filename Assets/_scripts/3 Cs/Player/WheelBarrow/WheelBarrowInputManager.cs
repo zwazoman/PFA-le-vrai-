556eb583ce2e9ef276bbd2f22349435c1c172bd3
zwazoman <141416442+zwazoman@users.noreply.github.com>
2024-06-07 16:10:21 +0200

    réparé la brouette de con de merde a chier ptn

--- Assets/_scripts/3 Cs/Player/WheelBarrow/WheelBarrowInputManager.cs
+++ Assets/_scripts/3 Cs/Player/WheelBarrow/WheelBarrowInputManager.cs
@@ -25,17 +25,20 @@ public class WheelBarrowInputManager : MonoBehaviour
     public event Action OnPause;
     public void Move(InputAction.CallbackContext context)
     {
+        if (!enabled) return;
         MoveInput = context.ReadValue<float>();
         if (context.performed) OnMove?.Invoke(); else if (context.canceled) OnStop?.Invoke();
     }
 
     public void Turn(InputAction.CallbackContext context)
     {
+        if (!enabled) return;
         TurnInput = context.ReadValue<float>();
     }
 
     public void Drop(InputAction.CallbackContext context)
     {
+        if (!enabled) return;
         if (context.performed)
         {
             OnDrop?.Invoke();
@@ -45,6 +48,7 @@ public class WheelBarrowInputManager : MonoBehaviour
 
     public void Empty(InputAction.CallbackContext context)
     {
+        if (!enabled) return;
         if (context.performed)
         {
             print("empty");
@@ -54,6 +58,7 @@ public class WheelBarrowInputManager : MonoBehaviour
 
     public void Sprint(InputAction.CallbackContext context)
     {
+        if (!enabled) return;
         if (context.performed)
         {
             OnStartSprint?.Invoke();
@@ -67,7 +72,16 @@ public class WheelBarrowInputManager : MonoBehaviour
     public void Pause(InputAction.CallbackContext context)
     {
         //if (!enabled) return;
-        print("putain la pause");
-        if (context.performed) OnPause?.Invoke();
+        if (context.performed) 
+        {
+            //print("con");
+            OnPause?.Invoke();
+        }
+        
+    }
+    private void OnDisable()
+    {
+        TurnInput = 0;
+        MoveInput = 0;
     }
 }
