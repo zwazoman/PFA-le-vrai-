148092ef6cc2baf58431b456e44030cee7c2615c
zwazoman <141416442+zwazoman@users.noreply.github.com>
2024-06-13 11:41:15 +0200

    trompinette good

--- Assets/TrompinetteSound.cs
+++ Assets/TrompinetteSound.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,38 +9,43 @@ public class TrompinetteSound : MonoBehaviour
     [SerializeField] AudioSource _stopAudioSource;
 
     [SerializeField] AudioClip[] _startEngine;
-    [SerializeField] float _startEngineVolume;
+    [SerializeField] float _startEngineVolume = 1f;
 
     [SerializeField] AudioClip[] _leaveEngine;
-    [SerializeField] float _leaveEngineVolume;
+    [SerializeField] float _leaveEngineVolume = 1f;
 
-    Rigidbody _playerRB;
+    WheelBarrowMovement _WBMove;
     float _playerWalkSpeed;
 
-    private void Start()
+    private void OnEnable()
     {
-        _playerRB = PlayerMain.Instance.GetComponent<Rigidbody>();
-        _playerWalkSpeed = PlayerMain.Instance.Stats.WalkSpeed;
-    }
-
-    private void Update()
-    {
-        _driveAudioSource.volume = _playerRB.velocity.magnitude / _playerWalkSpeed;
-        _stopAudioSource.volume = 1f - (_playerRB.velocity.magnitude / _playerWalkSpeed);
-     }
-
-    public void OnPickUp()
-    {
+        _WBMove = PlayerMain.Instance.WheelBarrow.Movement;
+        _playerWalkSpeed = PlayerMain.Instance.WheelBarrow.Movement._playerMoveSpeed;
         SFXManager.Instance.PlaySFXClip(_startEngine, transform.position, _startEngineVolume);
         _driveAudioSource.Play();
         _stopAudioSource.Play();
     }
 
-    public void OnDrop()
+    private void Update()
     {
+        _driveAudioSource.volume = _WBMove.Velocity.magnitude / _playerWalkSpeed;
+        _stopAudioSource.volume = 1f - (_WBMove.Velocity.magnitude / _playerWalkSpeed) + 0.3f;
+     }
+
+    public IEnumerator OnDrop()
+    {
+        _driveAudioSource.DOFade(0, 0.5f);
+        _stopAudioSource.DOFade(0, 0.5f);
+        yield return new WaitForSeconds(0.5f);
         SFXManager.Instance.PlaySFXClip(_leaveEngine, transform.position, _leaveEngineVolume);
         _driveAudioSource.Stop();
         _stopAudioSource.Stop();
+        this.enabled = false;
+    }
+
+    private void OnDisable()
+    {
+        StartCoroutine(OnDrop());
     }
 
 
